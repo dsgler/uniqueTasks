61fe6f9a57a88dd5db042ef5b9686e6281361b4b
dsgler <1832002325@qq.com>
2024-10-21 21:49:52 +0800

    层序打印

--- ts/binary-tree.ts
+++ ts/binary-tree.ts
@@ -31,6 +31,8 @@ function f(node: TreeNode | null,ans:number[]):void{
     f(node.right,ans);
 }
 
+// 中序遍历非递归版
+// 原理是用栈模拟递归
 function f2(root: TreeNode | null):number[]{
     if (root===null) return [];
 
@@ -60,4 +62,38 @@ let root=new TreeNode(1);
 root.left=new TreeNode(2)
 root.right=new TreeNode(3);
 
-console.log(f2(root))
+console.log(f2(root));
+
+// 层序打印
+function levelOrder(root: TreeNode | null): number[][] {
+    if (root===null) return [[]];
+
+    let ans:number[][]=[];
+    let queue:(TreeNode | null)[]=[];
+    let cur:TreeNode | null=root;
+
+    queue.push(cur);
+    let cnt=1;
+    while (cnt>0){
+        let ncnt=0;
+        let layer:number[]=[];
+        for (let i=0;i<cnt;i++){
+            cur=queue.shift() || null;
+            // cnt--;
+            if (cur!==null){
+                layer.push(cur.val);
+                queue.push(cur.left);
+                queue.push(cur.right);
+                ncnt+=2;
+            }
+        }
+        cnt=ncnt;
+        if (layer.length!==0){
+            ans.push(layer);
+        }
+    }
+
+    return ans;
+};
+
+console.log(levelOrder(root));
