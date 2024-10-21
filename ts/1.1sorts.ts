fa0ef3ed969d0ad86f667368cdf88a230fbe222f
dsgler <1832002325@qq.com>
2024-10-21 09:31:21 +0800

    heapSort

--- ts/1.1sorts.ts
+++ ts/1.1sorts.ts
@@ -16,3 +16,74 @@ function insertionSort(arr: number[]): void {
 const array = [5, 2, 9, 1, 5, 6,7,9999,22,2,-999];
 insertionSort(array)
 console.log(array);
+
+class heap{
+    private heapList:number[];
+    private heapLen:number;
+
+    constructor(arr:number[]){
+        this.heapList=arr;
+        this.heapLen=arr.length;
+        this.heapify();
+    }
+
+    private swap(i:number,j:number){
+        [this.heapList[i],this.heapList[j]]=[this.heapList[j],this.heapList[i]];
+    }
+
+    public heapify():void{
+        for (let process=1;process<this.heapLen;process++){
+            this.heapUp(process);
+        }
+
+    }
+
+    private heapUp(i:number):void{
+        if (i===0){
+            return;
+        }
+
+        let father=Math.floor((i-1)/2);
+        if (this.heapList[i]<this.heapList[father]){
+            this.swap(i,father);
+            this.heapUp(father);
+        }
+    }
+
+    public heapSort():void{
+        while (this.heapLen>=2){
+            this.swap(this.heapLen-1,0);
+            this.heapLen--;
+            this.heapDown(0);
+        }
+    }
+
+    private heapDown(i:number){
+        let left=2*i+1;
+        let right=left+1;
+        if(left>=this.heapLen) left=-1;
+        if(right>=this.heapLen) right=-1;
+        let target:number=-1;
+
+        if (left!==-1 && this.heapList[i]>this.heapList[left]){
+            if (right!==-1 && this.heapList[left]>this.heapList[right]){
+                target=right;
+            }else{
+                target=left;
+            }
+        }else if (right!==-1 && this.heapList[i]>this.heapList[right]){
+            target=right;
+        }
+
+        if (target===-1) return;
+
+        this.swap(i,target);
+        this.heapDown(target);
+    }
+}
+
+const array2 = [5, 2, 9, 1, 5, 6,7,9999,22,2,-999];
+let he=new heap(array2);
+// console.log(array2);
+he.heapSort();
+console.log(array2);
