92c423f78ed3c7284d1d3e7ae6e92e1ce1a26bfb
dsgler <1832002325@qq.com>
2024-10-22 15:24:38 +0800

    图上的拓扑排序dfs

--- ts/1.4grid.ts
+++ ts/1.4grid.ts
@@ -101,3 +101,62 @@ function bfs(i: number, j: number, record: boolean[][], grid: string[][]) {
     }
   }
 }
+
+/* ------------------------------------------------- */
+// https://leetcode.cn/problems/course-schedule/description/
+// 207. 课程表
+// 挺快的
+let num:number;
+function canFinish(numCourses: number, prerequisites: number[][]): boolean {
+  num=numCourses;
+  // 记录关系
+  let arr: number[][] = Array.from({ length: numCourses }, () => []);
+  // 记录经检验 可 的节点
+  let set:number[]=new Array(numCourses).fill(0);
+  
+  for (let ele of prerequisites){
+    arr[ele[0]].push(ele[1]);
+  }
+  
+  let cir:number[]=new Array(numCourses);
+  for (let i=0;i<numCourses;i++){
+    // 不用New清空数组而是fill的话，时间和空间都会好一点，特别是空间
+    cir.fill(0);
+    let res=dfs2(arr,set,cir,i);
+    if (res===true){
+      return false;
+    }
+  }
+  return true;
+};
+
+// 返回值是是否重复
+function dfs2(arr:number[][],set:number[],cir:number[],n:number):boolean{
+  // 只有一个节点的所有分支都可，这个节点才可
+  /* function writeback(){
+    for (let i=0;i<num;i++){
+      if (cir[i]===1) set[i]=1;
+    }
+  } */
+
+  if (set[n]!==0) {return false;}
+
+  if (cir[n]!==0) return true;
+
+  cir[n]=1;
+  // 走到头了，没有重复，回写
+  if (arr[n].length===0){return false;}
+
+  for (let k of arr[n]){
+    let res=dfs2(arr,set,cir,k);
+    if (res===true){
+      return true;
+    }
+    // 回复路径
+    cir[k]=0;
+  }
+  // 你过关！
+  set[n]=1;
+}
+
+console.log(canFinish(4,[[0,1],[1,2],[0,3],[3,0]]));
