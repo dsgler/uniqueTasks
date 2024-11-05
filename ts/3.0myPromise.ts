f03cbf816a1dac9966e75a5fc7632852100f0842
dsgler <1832002325@qq.com>
2024-11-05 12:28:37 +0800

    似乎能用了

--- ts/3.0myPromise.ts
+++ ts/3.0myPromise.ts
@@ -6,102 +6,188 @@ type CallbackType = (
 ) => any;
 
 interface thenable {
-  then: (resolve: (value: any) => void, reject: (reason: any) => void) => any;
+  then: (
+    onFulfilled: (value: any) => void,
+    onRejected: (reason: any) => void
+  ) => any;
 }
 
 let thenable = {
   then: function (resolve, reject) {
-    setTimeout(() => { resolve(42);console.log("time resolve") }, 1000);
+    setTimeout(() => {
+      resolve(42);
+      console.log("time resolve");
+    }, 1000);
   },
 };
 
-class myPromise {
-  private status: myPromiseStatus;
-  private resolveDate: any;
-  private rejectDate: any;
-
-  constructor(callback?: CallbackType) {
+class myPromise implements thenable {
+  status: myPromiseStatus;
+  value: any;
+  reason: any;
+  onResolvedCallbacks: Function[];
+  onRejectedCallbacks: Function[];
+  constructor(callback: CallbackType) {
     this.status = "pending";
+    this.onResolvedCallbacks = [];
+    this.onRejectedCallbacks = [];
     this.resolve = this.resolve.bind(this);
     this.reject = this.reject.bind(this);
-    this.resolveDate = null;
-    this.rejectDate = null;
-    callback && callback(this.resolve, this.reject);
+
+    try {
+      callback(this.resolve, this.reject);
+    } catch (e) {
+      this.reject(e);
+    }
   }
 
-  static resolve(value?: unknown): myPromise {
+  resolve(value: any) {
+    this.value = value;
+    this.status = "fulfilled";
+    this.onResolvedCallbacks.forEach((func) => func());
+  }
+
+  reject(reason: any) {
+    this.reason = reason;
+    this.status = "rejected";
+    this.onRejectedCallbacks.forEach((func) => func());
+  }
+
+  then(onFulfilled, onRejected?): myPromise {
+    onFulfilled =
+      typeof onFulfilled === "function" ? onFulfilled : (value: any) => value;
+    onRejected =
+      typeof onRejected === "function"
+        ? onRejected
+        : (err) => {
+            throw err;
+          };
+
+    let newPromise = new myPromise((resolve, reject) => {
+      if (this.status === "fulfilled") {
+        setTimeout(() => {
+          try {
+            let result = onFulfilled(this.value);
+            myPromise.recrusionPromise(newPromise, result, resolve, reject);
+          } catch (e) {
+            reject(e);
+          }
+        }, 0);
+      } else if (this.status === "rejected") {
+        setTimeout(() => {
+          try {
+            let result = onRejected(this.reason);
+            myPromise.recrusionPromise(newPromise, result, resolve, reject);
+          } catch (e) {
+            reject(e);
+          }
+        }, 0);
+      } else {
+        this.onResolvedCallbacks.push(() => {
+          setTimeout(() => {
+            try {
+              let result = onFulfilled(this.value);
+              myPromise.recrusionPromise(newPromise, result, resolve, reject);
+            } catch (e) {
+              reject(e);
+            }
+          }, 0);
+        });
+
+        this.onRejectedCallbacks.push(() => {
+          setTimeout(() => {
+            try {
+              let result = onRejected(this.reason);
+              myPromise.recrusionPromise(newPromise, result, resolve, reject);
+            } catch (e) {
+              reject(e);
+            }
+          }, 0);
+        });
+      }
+    });
+
+    return newPromise;
+  }
+
+  /**
+   * @describe 递归解决then的返回值的多种可能
+   * @param result 可能是thenable,那么就递归解决; 直接值，就包装后返回
+   * @returns
+   */
+  static recrusionPromise(
+    superPromise: myPromise,
+    result,
+    superResolve,
+    superReject
+  ) {
+    // 已经解决的不能再改变
+    if (superPromise.status !== "pending") {
+      return;
+    }
+
+    if (superPromise === result) {
+      superReject(TypeError("不能等待自己"));
+      return;
+    }
+
+    // if (result instanceof myPromise){
+    //   switch (result.status){
+    //     case "fulfilled":
+    //       myPromise.recrusionPromise(superPromise,result.value,superResolve,superReject);
+    //       break;
+    //     case "rejected":
+    //       myPromise.recrusionPromise(superPromise,result.reason,superResolve,superReject);
+    //       break;
+    //     case "pending":
+    //       result.resolve=(value)=>{
+    //         result.resolve.call(result,value);
+    //         console.log("修改resolve")
+    //         superResolve(value);
+    //       }
+    //       result.reject=(reason)=>{
+    //         result.reject.call(result,reason);
+    //         superReject(reason);
+    //       }
+    //   }
+    // }else
+    if (typeof result.then === "function") {
+      try {
+        (<thenable>result).then.call(result, superResolve, superReject);
+      } catch (e) {
+        superReject(e);
+      }
+    } else {
+      superResolve(result);
+    }
+  }
+
+  static resolve(value) {
     if (value instanceof myPromise) {
       return value;
     }
 
-    let ret: myPromise = new myPromise();
-
-    if (value && typeof value === "object" && "then" in <thenable>value) {
-      ret.status = "pending";
-      (<thenable>value).then(ret.resolve, ret.reject);
-      return ret;
+    if (typeof value.then === "function") {
+      return new myPromise(value.then);
     }
 
-    ret.status = "fulfilled";
-    ret.resolveDate = value;
-    ret.rejectDate = null;
-    return ret;
-  }
-
-  static reject(reason: any): myPromise {
-    let ret: myPromise = new myPromise();
-    ret.status = "rejected";
-    ret.rejectDate = reason;
-    return ret;
-  }
-
-  then(
-    then_resolve_func: (data: any) => any,
-    then_reject_func?: (reason: any) => any
-  ): myPromise {
-    if (this.status === "fulfilled") {
-      let ret;
-      if (then_resolve_func instanceof Function) {
-        ret = then_resolve_func(this.resolveDate);
-      }
-      if (ret instanceof myPromise) return ret;
-      return myPromise.resolve(ret);
-    } else if (this.status === "rejected") {
-      if (then_reject_func instanceof Function) {
-        then_reject_func(this.rejectDate);
-      }
-      return myPromise.reject(this.rejectDate);
-    } else {
-      this.then_resolve_func = then_resolve_func;
-      this.then_reject_func = then_reject_func;
-      return this;
-    }
-  }
-
-  resolve(value?: any) {
-    this.resolveDate = value;
-    this.status = "fulfilled";
-    if (this.then_resolve_func) {
-      this.then_resolve_func(value);
-    }
-  }
-
-  reject(reason?: any) {
-    this.rejectDate = reason;
-    this.status = "rejected";
-    if (this.then_reject_func) {
-      this.then_reject_func(reason);
-    }
+    return new myPromise((resolve) => resolve(value));
   }
 }
 
-let ppp = myPromise.resolve(thenable);
-ppp = ppp.then((data) => {
-  debugger;
+let aaa = myPromise.resolve(thenable);
+aaa = aaa.then((data) => {
   console.log(data);
-  return 2;
+  return new myPromise((resolve) => {
+    setTimeout(() => {
+      resolve("inner");
+    }, 2000);
+  });
 });
-ppp.then((data) => {
-  debugger;
+aaa=aaa.then((data) => {
   console.log(data);
+  return thenable
 });
+aaa.then((data)=>{
+  console.log(data)
+})
